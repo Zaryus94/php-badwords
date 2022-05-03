8ed02b3f3347a4a41d6ddd10e0d68fe2ff189f9a
Christian Podda <poddachristian94@gmail.com>
2022-05-03 16:17:17 +0200

    seconda parte: numero dei caratteri

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <?php
+
 // phpinfo();
 $text="prova di php per il corretto funzionamento"
+
  ?>
 
+<!-- stampa del testo e lenght -->
+
  <p>
+
  <?php
      echo($text);
      ?>
+
  </p>
+
+<p>
+    numero dei caratteri digitati: <?php echo(strlen($text)) ?>
+</p>
